22ad39a43c15ad2c036c96031f57ba6d085a3a92
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-12 19:51:44 -0400

    created ramp geo

--- src/Ramp.js
+++ src/Ramp.js
@@ -2,23 +2,47 @@ import * as THREE from 'three'
 import { GameObject } from './GameObject'
 // import { RapierPhysics } from 'three/examples/jsm/Addons.js';
 import { RapierPhysics } from './RapierPhysics'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
 
 export class RampObject extends GameObject{
   constructor(){
     super('Block')
     const radius = 2.5
-    const geometry = new THREE.TetrahedronGeometry( radius );
-    const material = new THREE.MeshPhongMaterial({
+    const loader = new GLTFLoader()
+
+    // const geometry = loader.load("public/models/ramp.glb", (ramp) => {
+    //   return ramp.scene
+    // })
+    // const geometry = await loader.loadAsync('public/models/ramp.glb')
+    // const geometry = loader.load("public/models/ramp.glb")
+    // const geometry = new THREE.TetrahedronGeometry( radius );
+    this.material = new THREE.MeshPhongMaterial({
       color: "green"
     })
 
-    this.mesh = new THREE.Mesh(geometry, material)
+    // this.mesh = geometry
+    // this.mesh = new THREE.Mesh(geometry, material)
+  }
+
+  async init() {
+    const loader = new GLTFLoader()
+    const geometry = await loader.loadAsync('/models/ramp.glb')
+    // const geometry = await loader.loadAsync('/models/cat_-_animated_low_poly.glb')
+    // debugger
+    // this.mesh = geometry.scene.children[0]
+    this.mesh = geometry.scene.children[0]
     this.mesh.position.y = 1.5
     this.mesh.position.x = 5
+    this.mesh.rotation.y = THREE.MathUtils.degToRad(180);
+    return this
   }
 
   initializePhysics(physics) {
-    physics.addMesh(this.mesh, 1)
+    physics.addMesh(this.mesh, 0, 0, (geometry) => {
+      const vertices = geometry.attributes.position.array;
+      return physics.RAPIER.ColliderDesc.convexHull(vertices);
+    })
+
     this.body = this.mesh.userData.physics?.body ?? null
 
     if (!this.body) throw new Error('Block physics body was not created')
@@ -60,7 +84,7 @@ export class RampObject extends GameObject{
     // } 
     // this.body.setLinearDamping(1.5)
     // this.body.setAngularDamping(1.5)
-    this.body.setGravityScale(10, true)
+    this.body.setGravityScale(1, true)
   }
 
   // this is directly from rapier
