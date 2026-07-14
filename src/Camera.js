03fadcfb62948095359e212c7d456aecba93a295
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-14 00:25:05 -0400

    set camera

--- src/Camera.js
+++ src/Camera.js
@@ -7,9 +7,8 @@ export class CameraObject{
     const near = 0.1;
     const far = 1000;
     this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-    this._currentPostition = new THREE.Vector3()
-    this._currentLookat = new THREE.Vector3()
+    // this.camera = new THREE.OrthographicCamera()
+    this.camera.position.set(10, 10 ,20)
   }
 
 
@@ -18,37 +17,13 @@ export class CameraObject{
   }
 
   update(time, target){
-    const cameraOffset = new THREE.Vector3(0, 5, 15); // 5 units up, 10 units behind
 
-    // 2. Get the character's world position
     const characterPosition = new THREE.Vector3();
-    target.getWorldPosition(characterPosition);
 
-    console.log(characterPosition)
-   
+    target.getWorldPosition(characterPosition);
     characterPosition.y = 2.5
 
-    // 3. Update the camera position
-    const ideal = this.camera.position.copy(characterPosition).add(cameraOffset);
 
-    // this.camera.position.lerp(ideal, 0.005)
-    // 4. Make the camera look at the character
-    this.camera.lookAt(characterPosition);
-
-    // Ensure the camera looks at the character
-    // this.camera.lookAt(characterPosition);
-    // const idealOffset = this._calculdateIdealOffset(target)
-    // const idealLookat = this._calculdateIdealLookat(target)
-
-    // this._currentPostition.copy(idealOffset)
-    // this._currentLookat.copy(idealLookat)
-
-    // this.camera.position.copy(this._currentPostition)
-    // this.camera.lookAt(this._currentLookat)
-  }
-
-
-  _lockOn(object) {
-    this.camera.lookAt(object.position)
+    this.camera.lookAt(characterPosition)
   }
 }
