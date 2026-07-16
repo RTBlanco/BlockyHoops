76d2ba5390dc5c09fdab172e715b8efa8523f7fa
RTBlanco <ronnytoribio1@hotmail.com>
2026-07-16 14:42:17 -0400

    updated camera movement

--- src/Camera.js
+++ src/Camera.js
@@ -11,13 +11,34 @@ export class CameraObject{
     // this.camera = new THREE.OrthographicCamera()
     this.camera.position.set(0, 7.8 ,15)
     // Camera Controls
+
+
+    this.idealOffset = new THREE.Vector3(0, 8, 20);
+    this.idealLookAt = new THREE.Vector3(); 
+
+
     const gui = new GUI()
     const cameraGui = gui.addFolder('Camera Postion')
-    cameraGui.add(this.camera.position, 'x', 0, 100)
-    cameraGui.add(this.camera.position, 'y', 0, 100)
-    cameraGui.add(this.camera.position, 'z', 0, 100)
+    cameraGui.add(this.camera.position, 'x', -50, 50)
+    cameraGui.add(this.camera.position, 'y', 50, 50)
+    cameraGui.add(this.camera.position, 'z', 50, 50)
+
+    const lookATgui = gui.addFolder('Lookat')
+    lookATgui.add(this.idealLookAt, 'x', -50, 50)
+    lookATgui.add(this.idealLookAt, 'y', -50, 50)
+    lookATgui.add(this.idealLookAt, 'z', -50, 50)
+
+    const offset = gui.addFolder('offset')
+    offset.add(this.idealOffset, 'x', -50, 50)
+    offset.add(this.idealOffset, 'y', -50, 50)
+    offset.add(this.idealOffset, 'z', -50, 50)
+    // offset.add(this.idealLookAt.rotation, 'y', -50,50)
   }
 
+  getCameraTargetPosition(character) {
+    const offset = this.idealOffset.clone();
+    return character.position.clone().add(offset);
+  }
 
   get mesh() {
     return this.camera
@@ -25,12 +46,15 @@ export class CameraObject{
 
   update(time, target){
 
-    const characterPosition = new THREE.Vector3();
+    const targetPosition = this.getCameraTargetPosition(target);
 
-    target.getWorldPosition(characterPosition);
-    characterPosition.y = 2.5
+    const t = 1.0 - Math.pow(0.001, time.getDelta())
 
+    this.camera.position.lerp(targetPosition, t);
+    
+    const currentLookAt = this.idealLookAt
+    currentLookAt.lerp(target.position, t)
 
-    this.camera.lookAt(characterPosition)
+    this.camera.lookAt(currentLookAt);
   }
 }
